4f2574282f4afd49d641f4496619b33df7d68201
vinsoyo <9416825+vinsoyo@users.noreply.github.com>
2018-10-22 23:52:37 -0400

    Ajout du Mock DBSet pour gagner du temps

--- MiamDbContext.cs
+++ MiamDbContext.cs
@@ -2,6 +2,23 @@
 
 namespace MIAM
 {
+    // https://docs.microsoft.com/en-us/ef/ef6/fundamentals/testing/mocking
+    //public class InMemoryMiamDbContext
+    //{
+    //    public InMemoryMiamDbContext(IList<Repas> repas)
+    //    {
+    //        IQueryable<Repas> data = repas.AsQueryable();
+    //        Mock<DbSet<Repas>> mockSet = new Mock<DbSet<Repas>>();
+    //        mockSet.As<IQueryable<Repas>>().Setup(m => m.Provider).Returns(data.Provider);
+    //        mockSet.As<IQueryable<Repas>>().Setup(m => m.Expression).Returns(data.Expression);
+    //        mockSet.As<IQueryable<Repas>>().Setup(m => m.ElementType).Returns(data.ElementType);
+    //        mockSet.As<IQueryable<Repas>>().Setup(m => m.GetEnumerator()).Returns(data.GetEnumerator());
+    //    }
+
+    //    public DbSet<Repas> Repas { get; set; }
+    //}
+
+
     public class MiamDbContext : DbContext
     {
 
